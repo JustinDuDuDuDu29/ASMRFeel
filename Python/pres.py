b8edbb75d143704dc55ea9a2e6197ae43c064e82
JustinDuDuDuDu29 <justinduchengkai@gmail.com>
2025-08-30 15:11:29 +0800

    finish testing arduino pressure

--- Python/pres.py
+++ Python/pres.py
@@ -27,24 +27,18 @@ def choose_port(default=None):
         idx = 0
     return ports[idx].device
 
-def pressureLEDxxx(stop_evt: Event, q:Queue):
+def pressureLED(stop_evt: Event, q:Queue):
     
     device.registers.set_led_mode(LedMode.INDIVIDUAL_MANUAL)
     while not stop_evt.is_set():
-        # if(q.empty()):
-        #     device.set_led_off()
-
         d = q.get()
-        print(d)
         d = d.split(",")
 
         for i, val in enumerate(d):
-            # print(f"{i}: {val}")
-            if int(val)>100:
+            if int(val)>60:
                 device.registers.set_individual_led(i, 255, 0, 0)
             else:
                 device.registers.set_individual_led(i, 0, 0, 0)
-            # sleep(.1)
 
 
 
@@ -55,33 +49,19 @@ def read_from_serial(stop_evt: Event, q:Queue, port: str, baud: int = 115200):
     while not stop_evt.is_set():
         try:
             with serial.Serial(port, baudrate=baud, timeout=1) as ser:
-                # Optional: set DTR/RTS here if your device needs it
                 ser.reset_input_buffer()
                 while not stop_evt.is_set():
                     try:
                         line = ser.readline()  # b'' on timeout
+
                         if line:
                             d = line.rstrip(b"\r\n").decode("utf-8")
-                            q.put_nowait(d)
-
-                            d = q.get()
-                            print(ser.in_waiting);
-                            print(f"d: {d}")
-
-                            d = d.split(",")
-
-                            for i, val in enumerate(d):
-                                # print(1)
-                                if int(val)>100:
-                                    device.set_led( 255, 0, 0, i)
-                                else:
-                                    device.registers.set_individual_led( 0, 0, 0,i)
+                            q.put(d)
                     except serial.SerialException as e:
                         print(f"[serial] read error: {e}; will reconnect")
-                        break  # break inner loop → outer reconnects
+                        break  
         except serial.SerialException as e:
             print(f"[serial] open error on {port}: {e}; retrying...")
-        # small backoff before reconnect
         stop_evt.wait(1.0)
 
 def main():
@@ -89,12 +69,12 @@ def main():
     port = choose_port()
     print(f"Starting connection at {port} {baud}…")
 
-    q = Queue(12)
+    q = Queue()
     stop_evt = multiprocessing.Event()
     t = threading.Thread(target=read_from_serial, args=(stop_evt, q, port, baud), daemon=True)
     t.start()
-    # p =Process(target=pressureLED, args=(stop_evt, q), daemon= True) 
-    # p.start()
+    p =Process(target=pressureLED, args=(stop_evt, q), daemon= True) 
+    p.start()
     print("Press 'q' then Enter to quit.")
     try:
         for line in sys.stdin:
@@ -106,7 +86,7 @@ def main():
     finally:
         stop_evt.set()
         t.join(timeout=2)
-        # p.join()
+        p.join()
         print("Stopped cleanly.")
 
 if __name__ == "__main__":
