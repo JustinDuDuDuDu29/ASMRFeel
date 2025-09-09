b16c0e35644b84b04ed12db7dac6e546a034e5c7
BrianGodd(黃永恩) <82190198+BrianGodd@users.noreply.github.com>
2025-09-10 05:41:36 +0800

    priority qcmd

--- Python/DataFeelProcess/DFHandler.py
+++ Python/DataFeelProcess/DFHandler.py
@@ -23,22 +23,16 @@ def Worker(stop_evt:Event, q_cmd: Queue):
         #     print(f"worker Err! {e}")
         #     stop_evt.set()
         #     break
-    
-    '''Cool Down'''
-    t0 = token(superDotID = 2, vibFrequency=20, vibIntensity=None, therIntensity=None, therDiff=-3, ledList=None)
-    t1 = token(superDotID = 3, vibFrequency=20, vibIntensity=None, therIntensity=None, therDiff=-3, ledList=None)
-    cmd = ("useToken", (t0, ))
-    cmd2 = ("useToken", (t1, ))
-    method, args = cmd
-    getattr(dfc, method)(*args)
-    method, args = cmd2
-    getattr(dfc, method)(*args)
 
 def Commander(stop_evt: Event, q_pres:Queue, q_vib:Queue, q_therm:Queue, q_cmd:Queue):
     start = False
     startCool = False
     last_trigger = time.perf_counter()
     lastCool_trigger = time.perf_counter()
+    time_priority = [0,0,0,0]
+    total_priority = [0,0,0,0]
+    last_state = []
+    TIMEPRIOIR_SCALE = 2
     while not stop_evt.is_set():
         print(q_pres.qsize(), q_vib.qsize(), q_therm.qsize())
         t = q_pres.get()
@@ -60,9 +54,7 @@ def Commander(stop_evt: Event, q_pres:Queue, q_vib:Queue, q_therm:Queue, q_cmd:Q
 
         if tone_smooth >= TONE_THRESHOLD:
             # print("PreHeating...")
-            # print("PreHeating...")
             if start == False:
-                # print("Heating Count")
                 # print("Heating Count")
                 last_trigger = time.perf_counter()
                 start = True
@@ -71,11 +63,10 @@ def Commander(stop_evt: Event, q_pres:Queue, q_vib:Queue, q_therm:Queue, q_cmd:Q
                 thermDiff = 4
         elif tone_smooth < TONE_THRESHOLD:
             # print("PreCooling...")
-            # print("PreCooling...")
             if startCool == False:
                 lastCool_trigger = time.perf_counter()
                 startCool = True
-            elif time.perf_counter() - lastCool_trigger >= DURATION_THRESHOLD*3:
+            elif time.perf_counter() - lastCool_trigger >= DURATION_THRESHOLD*10:
                 start = False
                 startCool = False
                 print("Cooling...")
@@ -96,13 +87,6 @@ def Commander(stop_evt: Event, q_pres:Queue, q_vib:Queue, q_therm:Queue, q_cmd:Q
                 # map int(val) from 0-1023 to 0-255
                 # led[i] = [int(val) // 4] * 3
                 led[i] = [255, 0, 0]
-        for i, val in enumerate(p):
-            if int(val)>60:
-                # led[i] = [random.randint(0,255), random.randint(0,255), random.randint(0,255)]
-                # led[i] = [int(val), int(val), int(val)]
-                # map int(val) from 0-1023 to 0-255
-                # led[i] = [int(val) // 4] * 3
-                led[i] = [255, 0, 0]
 
         led1 = [[0,0,0]]*8
 
@@ -113,32 +97,79 @@ def Commander(stop_evt: Event, q_pres:Queue, q_vib:Queue, q_therm:Queue, q_cmd:Q
                 # map int(val) from 0-1023 to 0-255
                 # led[i] = [int(val) // 4] * 3
                 led1[i] = [255, 0, 0]
-        for i, val in enumerate(p1):
-            if int(val)>60:
-                # led[i] = [random.randint(0,255), random.randint(0,255), random.randint(0,255)]
-                # led[i] = [int(val), int(val), int(val)]
-                # map int(val) from 0-1023 to 0-255
-                # led[i] = [int(val) // 4] * 3
-                led1[i] = [255, 0, 0]
 
 
         # print(vib, therm)
-        '''HeadPhone'''
-        t0 = token(superDotID = 2, vibFrequency=10, vibIntensity=vib, therIntensity=thermVal, therDiff=thermDiff, ledList=None)
-        t1 = token(superDotID = 3, vibFrequency=10, vibIntensity=vib, therIntensity=thermVal, therDiff=thermDiff, ledList=None)
-        '''Hand'''
-        t2 = token(superDotID = 0, vibFrequency=None, vibIntensity=None, therIntensity=None, therDiff=None, ledList=led)
-        t3 = token(superDotID = 1, vibFrequency=None, vibIntensity=None, therIntensity=None, therDiff=None, ledList=led1)
+        t0 = token(superDotID = 0, vibFrequency=70, vibIntensity=0, therIntensity=None, therDiff=0, ledList=led)
+        t1 = token(superDotID = 1, vibFrequency=70, vibIntensity=0, therIntensity=None, therDiff=0, ledList=led1)
+        t2 = token(superDotID = 2, vibFrequency=70, vibIntensity=vib, therIntensity=thermVal, therDiff=thermDiff, ledList=None)
+        t3 = token(superDotID = 3, vibFrequency=70, vibIntensity=vib, therIntensity=thermVal, therDiff=thermDiff, ledList=None)
+
         # print(t0)
         try:
-            q_cmd.put_nowait(("useToken", (t0, )))
-            q_cmd.put_nowait(("useToken", (t1, )))
-            q_cmd.put_nowait(("useToken", (t2, )))
-            q_cmd.put_nowait(("useToken", (t3, )))
-            q_cmd.put_nowait(("useToken", (t2, )))
-            q_cmd.put_nowait(("useToken", (t3, )))
-            # print(time.perf_counter()-last)
+            # print(f"qcmd: {q_cmd.qsize()}")
+            if len(last_state) != 0 :
+                '''t0'''
+                if t0.therDiff != last_state[0].therDiff: total_priority[0] += 100
+                if t0.vibIntensity != None and last_state[0].vibIntensity != None: 
+                    total_priority[0] += 5*abs(t0.vibIntensity - last_state[0].vibIntensity) 
+                if t0.ledList is not None:
+                    total_priority[0] += (int)((sum(abs(x - y) for row1, row2 in zip(t0.ledList, last_state[0].ledList) for x, y in zip(row1, row2)))/255)
+                '''t1'''
+                if t1.therDiff != last_state[1].therDiff: total_priority[1] += 100
+                if t1.vibIntensity != None and last_state[1].vibIntensity != None: 
+                    total_priority[1] += 5*abs(t1.vibIntensity - last_state[1].vibIntensity) 
+                if t1.ledList is not None:
+                    total_priority[1] += (int)((sum(abs(x - y) for row1, row2 in zip(t1.ledList, last_state[1].ledList) for x, y in zip(row1, row2)))/255)
+                '''t2'''
+                if t2.therDiff != last_state[2].therDiff: total_priority[2] += 100
+                if t2.vibIntensity != None and last_state[2].vibIntensity != None: 
+                    total_priority[2] += 5*abs(t2.vibIntensity - last_state[2].vibIntensity) 
+                if t2.ledList is not None:
+                    total_priority[2] += (int)((sum(abs(x - y) for row1, row2 in zip(t2.ledList, last_state[2].ledList) for x, y in zip(row1, row2)))/255)
+                '''t3'''
+                if t3.therDiff != last_state[3].therDiff: total_priority[3] += 100
+                if t3.vibIntensity != None and last_state[3].vibIntensity != None: 
+                    total_priority[3] += 5*abs(t3.vibIntensity - last_state[3].vibIntensity) 
+                if t3.ledList is not None:
+                    total_priority[3] += (int)((sum(abs(x - y) for row1, row2 in zip(t3.ledList, last_state[3].ledList) for x, y in zip(row1, row2)))/255)
+            
+
+            if max(total_priority) == total_priority[0]:
+                q_cmd.put_nowait(("useToken", (t0, )))
+                # print(t0)
+                time_priority[0] = 0
+                time_priority[1] += TIMEPRIOIR_SCALE
+                time_priority[2] += TIMEPRIOIR_SCALE
+                time_priority[3] += TIMEPRIOIR_SCALE
+            elif max(total_priority) == total_priority[1]:
+                q_cmd.put_nowait(("useToken", (t1, )))
+                # print(t1)
+                time_priority[0] += TIMEPRIOIR_SCALE
+                time_priority[1] = 0
+                time_priority[2] += TIMEPRIOIR_SCALE
+                time_priority[3] += TIMEPRIOIR_SCALE
+            elif max(total_priority) == total_priority[2]:
+                q_cmd.put_nowait(("useToken", (t2, )))
+                # print(t2)
+                time_priority[0] += TIMEPRIOIR_SCALE
+                time_priority[1] += TIMEPRIOIR_SCALE
+                time_priority[2] = 0
+                time_priority[3] += TIMEPRIOIR_SCALE
+            elif max(total_priority) == total_priority[3]:
+                q_cmd.put_nowait(("useToken", (t3, )))
+                # print(t3)
+                time_priority[0] += TIMEPRIOIR_SCALE
+                time_priority[1] += TIMEPRIOIR_SCALE
+                time_priority[2] += TIMEPRIOIR_SCALE
+                time_priority[3] = 0
+            
+            last_state.clear()
+            last_state.append(t0)
+            last_state.append(t1)
+            last_state.append(t2)
+            last_state.append(t3)
+            total_priority = time_priority
 
         except pyqueue.Full:
             print("q_cmd full!!!")
-    
